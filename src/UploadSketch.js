e1bb3a0c49ef6456d10886e62b5e715ad7f6c711
nikkifang <nikkifang@tencent.com>
2019-12-09 21:03:05 +0800

    modify uploadSketch

--- src/UploadSketch.js
+++ src/UploadSketch.js
@@ -1,49 +1,57 @@
 import * as fs from "@skpm/fs";
 
 import BrowserWindow from "sketch-module-web-view";
-import { getWebview } from "sketch-module-web-view/remote";
+import {
+  getWebview
+} from "sketch-module-web-view/remote";
 import UI from "sketch/ui";
 import sketch from "sketch";
 import * as util from "./util";
-import { generateHtml } from "./GenerateHtml";
+import {
+  generateHtml
+} from "./GenerateHtml";
 
 const webviewIdentifier = "sketchresourcehub.webview";
 const Document = require("sketch/dom").Document;
-export default function() {
+export default function () {
   const document = require("sketch/dom").getSelectedDocument();
   const documentId = document.id;
   const documentName = decodeURIComponent(
     document.path.substr(document.path.lastIndexOf("/") + 1)
   );
+  console.log(documentId);
 
   const fileHash = String(
     NSFileManager.defaultManager()
-      .contentsAtPath(decodeURIComponent(document.path))
-      .sha1AsString()
+    .contentsAtPath(decodeURIComponent(document.path))
+    .sha1AsString()
   );
 
   //using system temp path;
   const tmpPath = "/tmp/" + documentId + "_" + fileHash;
   const zipUrl = tmpPath + ".zip";
 
-  //TODO 判断文件夹已存在，不再重复导出
-
+  //TODO 判断文件夹已存er.alloc().initWithValue_(false);
+  let isDir = MOPointer.alloc().initWithValue_(false);
   const previewPath = tmpPath + "/preview/";
-  
-  const imgIds = [];
 
-  //export page preview
-  let scale = 0.5;
-  sketch.export(document.pages, {
-    output: previewPath,
-    format: "png",
-    "save-for-web": true,
-    "use-id-for-name": true,
-    scales: scale // preview img compress
-  });
+  if(!NSFileManager.defaultManager().fileExistsAtPath_isDirectory(tmpPath, isDir)){
+    //export page preview
+    sketch.export(document.pages, {
+      output: previewPath,
+      format: "png",
+      "save-for-web": true,
+      "use-id-for-name": true,
+      scales: 0.5 // preview img compress
+    });
+  }else{
+    //TODO remove sketch file to reduce the zip size
+  }
 
+  let imgAll = [];
   let previewObj = {
-    id: documentId,
+    documentId: documentId,
+    md5: fileHash,
     selected: [],
     all: []
   };
@@ -51,8 +59,8 @@ export default function() {
   document.pages.forEach(page => {
     //skip symbol master page
     if (page.isSymbolsPage()) return;
-    imgIds.push(page.id); //记录所有page的id
-    let previewImg = previewPath + page.id + "@" + scale + "x.png";
+    imgAll.push(page.id); //记录所有page的id
+    let previewImg = previewPath + page.id + "@0.5x.png";
     let url = NSURL.fileURLWithPath(previewImg),
       bitmap = NSData.alloc().initWithContentsOfURL(url),
       base64 = bitmap.base64EncodedStringWithOptions(0) + "";
@@ -93,12 +101,17 @@ export default function() {
       .catch(console.error);
   });
 
+
   webContents.on("sketchUpload", s => {
+    // 交互or视觉
     let type = s.type || 1;
+    // 选中页面or全部页面
     let selected = s.page || "selected";
-
+    let uploadTag = s.uploadTag; // 是否已上传过 （不再重复上传sketch源文件）
     let sketchFileUrl = tmpPath + "/" + documentName;
+    let imgIds;
 
+    // 导出上传页面两倍缩略图（文件大时耗时长，尽量单个页面上传）
     console.time("export");
     if (selected === "selected") {
       sketch.export(document.selectedPage, {
@@ -117,18 +130,40 @@ export default function() {
         "use-id-for-name": true,
         scales: 2
       });
+      imgIds = imgAll;
     }
     console.timeEnd("export");
 
-    util.saveSketchFile([document.path, sketchFileUrl]).then(() => {
-      if (type === 2) {
-        // 设计稿导出标注
-        // 导出symbol icons
-        let symbols = util.findPagesMaster(context);
-        util.mkdirpSync(tmpPath + "/symbolpng");
-        util.mkdirpSync(tmpPath + "/symbolsvg");
-      }
+    // TODO 优化优化
+    if (!uploadTag) {
+      // 复制sketch文件到上传目录下
+      util.saveSketchFile([decodeURIComponent(document.path), sketchFileUrl]).then(() => {
+        if (type === 2) {
+          //TODO 设计稿导出标注
+          //TODO 导出symbol icons
+          let symbols = util.findPagesMaster(context);
+          util.mkdirpSync(tmpPath + "/symbolpng");
+          util.mkdirpSync(tmpPath + "/symbolsvg");
+        }
 
+        // 压缩上传到web
+        util.zipSketch([zipUrl, tmpPath]).then(() => {
+          let data = util.encodeBase64(zipUrl);
+          webContents
+            .executeJavaScript(
+              `callSketchUpload(${JSON.stringify({
+                documentId: documentId,
+                md5: fileHash,
+                sketchContent: data,
+                sketchName: documentName,
+                imgIds: imgIds
+              })})`
+            )
+            .catch(console.error);
+        });
+      });
+    } else {
+      // 压缩上传到web
       util.zipSketch([zipUrl, tmpPath]).then(() => {
         let data = util.encodeBase64(zipUrl);
         webContents
@@ -143,7 +178,8 @@ export default function() {
           )
           .catch(console.error);
       });
-    });
+    }
+
 
     // util.saveSketchFile([path, sketchFileUrl]).then(() => {
     //   const fileHash = String(
