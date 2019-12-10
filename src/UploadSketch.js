25babe685f72843a29ebd6f0f20c4cdfabec68f3
nikkifang <nikkifang@tencent.com>
2019-12-10 17:32:22 +0800

    主动保存文件

--- src/UploadSketch.js
+++ src/UploadSketch.js
@@ -21,60 +21,6 @@ export default function () {
   );
   console.log(documentId);
 
-  const fileHash = String(
-    NSFileManager.defaultManager()
-    .contentsAtPath(decodeURIComponent(document.path))
-    .sha1AsString()
-  );
-
-  //using system temp path;
-  const tmpPath = "/tmp/" + documentId + "_" + fileHash;
-  const zipUrl = tmpPath + ".zip";
-
-  //TODO 判断文件夹已存er.alloc().initWithValue_(false);
-  let isDir = MOPointer.alloc().initWithValue_(false);
-  const previewPath = tmpPath + "/preview/";
-
-  if(!NSFileManager.defaultManager().fileExistsAtPath_isDirectory(tmpPath, isDir)){
-    //export page preview
-    sketch.export(document.pages, {
-      output: previewPath,
-      format: "png",
-      "save-for-web": true,
-      "use-id-for-name": true,
-      scales: 0.5 // preview img compress
-    });
-  }else{
-    //TODO remove sketch file to reduce the zip size
-  }
-
-  let imgAll = [];
-  let previewObj = {
-    documentId: documentId,
-    md5: fileHash,
-    selected: [],
-    all: []
-  };
-  //format data
-  document.pages.forEach(page => {
-    //skip symbol master page
-    if (page.isSymbolsPage()) return;
-    imgAll.push(page.id); //记录所有page的id
-    let previewImg = previewPath + page.id + "@0.5x.png";
-    let url = NSURL.fileURLWithPath(previewImg),
-      bitmap = NSData.alloc().initWithContentsOfURL(url),
-      base64 = bitmap.base64EncodedStringWithOptions(0) + "";
-
-    let img = {
-      name: page.id,
-      base64: "data:image/jpg;base64," + base64
-    };
-    previewObj.all.push(img);
-    if (page.selected) {
-      previewObj.selected.push(img);
-    }
-  });
-
   const options = {
     parent: sketch.getSelectedDocument(),
     modal: true,
@@ -89,6 +35,60 @@ export default function () {
   };
 
   const browserWindow = new BrowserWindow(options);
+  let fileHash;
+  let imgAll = [];
+  let tmpPath, zipUrl, previewPath;
+
+  let previewObj = {
+    documentId: documentId,
+    md5: "",
+    selected: [],
+    all: []
+  };
+  //先保存文件, 每次保存文件hash会变化
+  document.save(err => {
+
+    fileHash = String(
+      NSFileManager.defaultManager()
+      .contentsAtPath(decodeURIComponent(document.path))
+      .sha1AsString()
+    );
+    tmpPath = "/tmp/" + documentId + "_" + fileHash;
+    previewObj.md5 = fileHash;
+    zipUrl = tmpPath + ".zip";
+    previewPath = tmpPath + "/preview/";
+
+    //export page preview
+    sketch.export(document.pages, {
+      output: previewPath,
+      "save-for-web": true,
+      "use-id-for-name": true,
+      scales: 0.2 // preview img compress
+    });
+
+    document.pages.forEach(page => {
+      //skip symbol master page
+      if (page.isSymbolsPage()) return;
+      imgAll.push(page.id); //记录所有page的id
+      let previewImg = previewPath + page.id + "@0.2x.png";
+      let url = NSURL.fileURLWithPath(previewImg),
+        bitmap = NSData.alloc().initWithContentsOfURL(url),
+        base64 = bitmap.base64EncodedStringWithOptions(0) + "";
+
+      let img = {
+        name: page.id,
+        base64: "data:image/jpg;base64," + base64
+      };
+      previewObj.all.push(img);
+      if (page.selected) {
+        previewObj.selected.push(img);
+      }
+    });
+
+    // browserWindow.loadURL('https://wedesign.oa.com/UploadSketch?sketch=1');
+    browserWindow.loadURL("http://localhost:8081/UploadSketch?sketch=1");
+  });
+
 
   browserWindow.once("ready-to-show", () => {
     browserWindow.show();
@@ -107,7 +107,6 @@ export default function () {
     let type = s.type || 1;
     // 选中页面or全部页面
     let selected = s.page || "selected";
-    let uploadTag = s.uploadTag; // 是否已上传过 （不再重复上传sketch源文件）
     let sketchFileUrl = tmpPath + "/" + documentName;
     let imgIds;
 
@@ -116,53 +115,32 @@ export default function () {
     if (selected === "selected") {
       sketch.export(document.selectedPage, {
         output: previewPath,
-        format: "png",
         "save-for-web": true,
         "use-id-for-name": true,
-        scales: 2
+        scales: 1
       });
       imgIds = [document.selectedPage.id];
     } else {
       sketch.export(document.pages, {
         output: previewPath,
-        format: "png",
         "save-for-web": true,
         "use-id-for-name": true,
-        scales: 2
+        scales: 1,
       });
       imgIds = imgAll;
     }
     console.timeEnd("export");
 
-    // TODO 优化优化
-    if (!uploadTag) {
-      // 复制sketch文件到上传目录下
-      util.saveSketchFile([decodeURIComponent(document.path), sketchFileUrl]).then(() => {
-        if (type === 2) {
-          //TODO 设计稿导出标注
-          //TODO 导出symbol icons
-          let symbols = util.findPagesMaster(context);
-          util.mkdirpSync(tmpPath + "/symbolpng");
-          util.mkdirpSync(tmpPath + "/symbolsvg");
-        }
 
-        // 压缩上传到web
-        util.zipSketch([zipUrl, tmpPath]).then(() => {
-          let data = util.encodeBase64(zipUrl);
-          webContents
-            .executeJavaScript(
-              `callSketchUpload(${JSON.stringify({
-                documentId: documentId,
-                md5: fileHash,
-                sketchContent: data,
-                sketchName: documentName,
-                imgIds: imgIds
-              })})`
-            )
-            .catch(console.error);
-        });
-      });
-    } else {
+    if (type === 2) {
+      //TODO 设计稿导出标注
+      //TODO 导出symbol icons
+      let symbols = util.findPagesMaster(context);
+      util.mkdirpSync(tmpPath + "/symbolpng");
+      util.mkdirpSync(tmpPath + "/symbolsvg");
+    }
+
+    util.saveSketchFile([decodeURIComponent(document.path), sketchFileUrl]).then(() => {
       // 压缩上传到web
       util.zipSketch([zipUrl, tmpPath]).then(() => {
         let data = util.encodeBase64(zipUrl);
@@ -178,7 +156,7 @@ export default function () {
           )
           .catch(console.error);
       });
-    }
+    });
 
 
     // util.saveSketchFile([path, sketchFileUrl]).then(() => {
@@ -229,8 +207,6 @@ export default function () {
     browserWindow.close();
   });
 
-  // browserWindow.loadURL('https://wedesign.oa.com/UploadSketch?sketch=1');
-  browserWindow.loadURL("http://localhost:8081/UploadSketch?sketch=1");
 }
 
 export function onShutdown() {
